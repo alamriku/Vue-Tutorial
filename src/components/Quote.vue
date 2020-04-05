e8a5d848f1ad908d42f560079b2b591a780ea7ec
Riku <alamriku069@gmail.com>
2020-04-05 23:25:50 +0600

    Passing Content The Suboptimal Solution,Passing Content with Slots How Slot Content gets Compiled and Styled 8. Default Slots and Slot Defaults

---
+++ src/components/Quote.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+  <div class="title">
+    <slot name="title"></slot>
+    <span style="color: #cccccc"><slot name="subtitle">The Subtitle</slot></span>
+  </div>
+    <hr>
+    <slot name="content"></slot>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+div{
+  border: 1px solid #ccc;
+  box-shadow: 1px 1px 2px 2px black;
+  padding: 30px;
+  margin: 30px auto;
+  text-align: center;
+}
+h1{
+  color: red;
+}
+  .title{
+    font-family: "Rage Italic";
+    font-palette: dark;
+    font-style: italic;
+  }
+</style>
