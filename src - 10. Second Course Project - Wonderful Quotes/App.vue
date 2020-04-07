dc0e05faf7adebcce2c6ba61aa63711a54d79154
Riku <alamriku069@gmail.com>
2020-04-08 01:42:18 +0600

    11. Handling User Input with Forms:

---
+++ src - 10. Second Course Project - Wonderful Quotes/App.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container">
+      <app-header :quote-count="quotes.length" :max-quote="maxQuotes"></app-header>
+      <app-new-quote @quoteAdded="newQuote($event)" ></app-new-quote>
+      <app-quote-grid :quotes="quotes" @quoteDeleted ="deleteQuote"></app-quote-grid>
+      <div class="row">
+        <div class="col-sm-12 text-center">
+          <div class="alert alert-info">Info:Click on a Quote to Delete it!</div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+import QuoteGrid from './components/QuoteGrid'
+import NewQuote from './components/NewQuote'
+import Header from './components/Header'
+export default {
+  data: function () {
+    return {
+      maxQuotes: 10,
+      quotes: [
+        'just a quote to see something'
+      ]
+    }
+  },
+  methods: {
+    newQuote (quote) {
+      if (this.quotes.length >= this.maxQuotes) {
+        return alert('Please delete Quotes first')
+      }
+      this.quotes.push(quote)
+    },
+    deleteQuote (index) {
+      this.quotes.splice(index, 1)
+    }
+  },
+  components: {
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote,
+    appHeader: Header
+  }
+}
+</script>
+
+<style>
+</style>
