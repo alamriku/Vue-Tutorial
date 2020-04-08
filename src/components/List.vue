4fd1e1f7333bcf28931d8903d779c038a0f1a939
Riku <alamriku069@gmail.com>
2020-04-08 18:06:51 +0600

    Understanding Mixins,Creating and Using Mixins,How Mixins get Merged,Creating a Global Mixin (Special Case), Mixins and Scope

---
+++ src/components/List.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="">
+    <h1>Filters & Mixins</h1>
+
+    <hr>
+    <input type="text" v-model="filterText">
+    <ul>
+      <li v-for="fruit in filteredFruits" v-bind:key="fruit.index">{{fruit}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import {fruitMixin} from '../fruitMixin'
+
+export default {
+  mixins: [fruitMixin],
+  created () {
+    console.log('Inside List Created HOok')
+  }
+}
+</script>
+
+<style>
+
+</style>
