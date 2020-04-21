605e0f683570c10a24e9a164ad72f3476e2165ba
Riku <alamriku069@gmail.com>
2020-04-21 21:07:24 +0600

    5. Navigating with Router Links

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav class="nav nav-pills flex-column flex-sm-row">
+    <ul class="nav nav-pills nav-fill">
+      <li class="nav-item">
+        <router-link to="/" >Home</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link to="/user" >User</router-link>
+      </li>
+    </ul>
+
+   </nav>
+</template>
