aabed275bb240387e16aa7fd074419ee786c8878
Riku <alamriku069@gmail.com>
2020-04-21 23:09:09 +0600

    18. Redirecting

--- src/route.js
+++ src/route.js
@@ -24,5 +24,9 @@ export const routes = [
       {path: '', component: UserStart},
       {path: ':id', component: UserDetail},
       {path: ':id/edit', component: UserEdit, name: 'userEdit'}
-    ]}
+    ]},
+  {
+    path: '/redirect-me',
+    redirect: { name: 'home' }
+  }
 ]
