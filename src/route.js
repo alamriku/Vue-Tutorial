033227bb0c654e2b098482a571684dbb16b08f19
Riku <alamriku069@gmail.com>
2020-04-21 23:04:03 +0600

    16. Using Query Parameters,17. Multiple Router Views (Named Router Views)

--- src/route.js
+++ src/route.js
@@ -1,13 +1,25 @@
 import User from './components/user/User'
 import Home from './components/Home'
+import Header from './components/Header'
 import UserStart from './components/user/UserStart'
 import UserDetail from './components/user/UserDetail'
 import UserEdit from './components/user/UserEdit'
 export const routes = [
 // if it is domain ' ' then home is used
-  {path: '', component: Home, name: 'home'},
-  {path: '/user',
-    component: User,
+  {
+    path: '',
+    name: 'home',
+    components: {
+      default: Home,
+      'header-top': Header
+    }
+  },
+  {
+    path: '/user',
+    components: {
+      default: User,
+      'header-bottom': Header
+    },
     children: [
       {path: '', component: UserStart},
       {path: ':id', component: UserDetail},
