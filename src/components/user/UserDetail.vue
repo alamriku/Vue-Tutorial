5ac8e469ebf047df77cbef3f9195000a930f626f
Riku <alamriku069@gmail.com>
2020-04-21 22:28:15 +0600

    13. Navigating to Nested Routes,14. Making Router Links more Dynamic

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -2,5 +2,10 @@
   <div>
     <h3>Some user Details</h3>
     <p>User Loaded Has ID:{{$route.params.id}}</p>
+    <router-link
+      tag="button"
+      class="btn btn-secondary"
+      :to="'/user/'+$route.params.id+'/edit'"
+    >Edit User</router-link>
   </div>
 </template>
