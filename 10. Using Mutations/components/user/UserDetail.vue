a0a62b81be1deac351dccb16536c32db96cc9bd5
Riku <alamriku069@gmail.com>
2020-04-22 16:59:35 +0600

    10. Using Mutations

---
+++ 10. Using Mutations/components/user/UserDetail.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h3>Some user Details</h3>
+
+    <router-link
+      tag="button"
+      class="btn btn-secondary"
+      :to="link"
+    >Edit User</router-link>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      link: {
+        name: 'userEdit',
+        params: {
+          id: this.$route.params.id
+        },
+        query: {
+          locale: 'en', q: 100
+        },
+        hash: '#data'
+      }
+    }
+  },
+  beforeRouteEnter (to, from, next) {
+    // if authenticate
+    // eslint-disable-next-line no-constant-condition
+    // if (false) {
+    //   next()
+    // } else {
+    //   next(false)
+    // }
+    // eslint-disable-next-line no-constant-condition
+    if (true) {
+      next()
+    } else {
+      next(false)
+    }
+  }
+}
+</script>
