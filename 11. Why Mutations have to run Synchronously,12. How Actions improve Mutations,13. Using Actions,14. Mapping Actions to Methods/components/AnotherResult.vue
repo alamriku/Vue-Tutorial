b9f2f08408691cb57d852fdaedb18e5c8f0a181e
Riku <alamriku069@gmail.com>
2020-04-22 18:05:38 +0600

    11. Why Mutations have to run Synchronously,12. How Actions improve Mutations,13. Using Actions,14. Mapping Actions to Methods

---
+++ 11. Why Mutations have to run Synchronously,12. How Actions improve Mutations,13. Using Actions,14. Mapping Actions to Methods/components/AnotherResult.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>Counter is: {{ doubleCounter }}</p>
+    <p>Number of clicks {{ stringCounter }}</p>
+  </div>
+
+</template>
+
+<script>
+import {mapGetters} from 'vuex'
+export default {
+  computed: {
+    // here three dots means spread operation in es-6
+    ...mapGetters([
+      'doubleCounter',
+      'stringCounter'
+    ]),
+    ourOwnComputedproperty () {
+      return true
+    }
+  }
+
+}
+</script>
